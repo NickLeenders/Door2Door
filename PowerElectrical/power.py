05b2e42c5ade7b129a5aa836b26444b2556de9fc
Matheus Coelho Silva <msilva95@ufl.edu>
2019-05-28 10:03:35 +0200

    Finalized aero.py calculations

--- PowerElectrical/power.py
+++ PowerElectrical/power.py
@@ -4,7 +4,7 @@ import Aerodynamics.aero as aero
 
 class ThrustCalculator:
     """Preliminary required thrust calculation for one phase"""
-    b = 8.8
+    b = 8.44
     c_avg = 1.2
     S = b*c_avg
     A = b/c_avg
@@ -14,14 +14,14 @@ class ThrustCalculator:
     mu = 0.015
     range = 400000
 
-    def __init__(self, velocity, altitude, duration, rateOfClimb=0, acceleration=0, driving=0):
+    def __init__(self, velocity, altitude, duration, rateOfClimb=0.0, acceleration=0.0, driving=0):
         self.velocity = velocity
         self.altitude = altitude
         self.rho, self.T, self.p = isa.IsaCalculator(altitude)
         self.duration = duration
-        if rateOfClimb == 0:
+        if abs(rateOfClimb) < 1.0E-5:
             self.cl = (self.mass*9.80665)/(self.S*0.5*self.rho*self.velocity**2)
-        elif rateOfClimb > 0:
+        elif rateOfClimb > 1.0E-5:
             self.cl = math.sqrt(3*math.pi*self.cd0*self.A*self.e)
         self.drag = aero.drag(self.cd0, self.cl, self.b, self.c_avg, self.e, self.velocity, self.rho)
         self.acceleration = acceleration
@@ -33,21 +33,35 @@ class ThrustCalculator:
 
 
 def main():
-    drive1 = ThrustCalculator(29.0, 0.0, 50000.0/29.0, 0, 0, 1)
+    #drive1_t = ThrustCalculator(29.0, 0.0, 50000.0/29.0, 0, 0, 1)
 
-    takeOff = ThrustCalculator(36.0, 0.0, 1500/2.5, 0, 4.0/3.0, 1)
+    takeOff_t = ThrustCalculator(36.0, 0.0, 1500 / 2.5, 0, 4.0 / 3.0, 1)
+    takeOff_l = aero.Propellers(takeOff_t.thrust, takeOff_t.velocity,
+                                takeOff_t.rho, takeOff_t.cl, 1)
 
-    climb = ThrustCalculator(2*takeOff.mass*9.80665/(takeOff.S*takeOff.rho*math.sqrt(3*math.pi*takeOff.cd0*takeOff.A*takeOff.e)), 750, 1500/7.44)
+    print(takeOff_l.lift_powered)
+    print(takeOff_t.mass*9.80665)
+    # iterating = True
+    # while (iterating):
+    #     takeOff_t = ThrustCalculator(36.0, 0.0, 1500/2.5, 0, 4.0/3.0, 1)
+    #     takeOff_l = aero.Propellers(takeOff_t.thrust, takeOff_t.velocity,
+    #                                       takeOff_t.rho, takeOff_t.cl, 0)
+    #     if(takeOff_l.lift_powered < takeOff_t.mass*9.80665):
 
-    cruise = ThrustCalculator(69.4, 1500, 400000/69.4 + climb.duration)
 
-    reserve = ThrustCalculator(69.0, 1500, 45*60)
+    #climb = ThrustCalculator(2*takeOff.mass*9.80665/(takeOff.S*takeOff.rho*math.sqrt(3*math.pi*takeOff.cd0*takeOff.A*takeOff.e)), 750, 1500/7.44)
 
-    landStart = ThrustCalculator(31.0, 1500, 600)
+    #cruise = ThrustCalculator(69.4, 1500, 400000/69.4 + climb.duration)
 
-    landing = ThrustCalculator(31.0, 750, 1500/(31.0*sin(math.atan(1.0/3.0))))
+    #reserve = ThrustCalculator(69.0, 1500, 45*60)
 
-    drive2 = ThrustCalculator(29.0, 0.0, 50000.0 / 29.0, 0, 0, 1)
+    #landStart = ThrustCalculator(31.0, 1500, 600)
+
+    #landing = ThrustCalculator(31.0, 750, 1500/(31.0*sin(math.atan(1.0/3.0))))
+
+    #drive2 = ThrustCalculator(29.0, 0.0, 50000.0 / 29.0, 0, 0, 1)
+
+    return
 
 
 if __name__ == "__main__":
