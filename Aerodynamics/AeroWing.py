829846a3c1d3b058fa6a5162d7b9c7aa02f4f1d9
fleurverschoor <38730051+fleurverschoor@users.noreply.github.com>
2019-05-28 12:27:25 +0200

    Added aero values

---
+++ Aerodynamics/AeroWing.py
@@ -0,0 +1,50 @@
+Masstotal=1558.0 #kg
+Ws_cr=Masstotal*9.80665 #N
+We_cr= #N
+
+
+
+#variables
+sweepquart=0 #deg
+taper=1 #-
+dihedral=0 #deg
+twist=0 #deg/m
+vflow_takeoff=  #m/s
+vflow_cr=  #m/s
+clmax=1.5
+
+b= 4.4 #m
+c= 1.2 #m
+######
+
+# ISA
+def ISA(h, R, lapse, g, T0, rho0):
+    T_cr=T0-lapse*h
+    rho_cr=rho0*(T_cr/T0)**(g/(lapse*R)-1)
+    return(T_cr, rho_cr)
+######
+
+# Extra velocities and q's
+v_takeoff=vflow+vinf_takeoff
+v_cr=vflow+vinf_cr
+q0=0.5*rho0*v_takeoff**2
+q_cr=0.5*rho_cr*v_cr**2
+#####
+
+# Sreq and design cl
+Sreq=Ws_cr/(q0*cl_max)
+cl_des=0.5*(Ws_cr+We_cr)/(q_cr*Sreq)
+#####
+
+# Mach and Reynolds
+Mach_takeoff=v_takeoff/((gamma*R*T0)**0.5)
+Re_takeoff=rho0*v_takeoff*c/mu
+
+Mach_cr=v_cr/((gamma*R*T_cr)**0.5)
+Re_cr=rho_cr*v_cr*c/mu
+#####
+
+cd_cr=0.05
+
+wingdrag= q_cr*Sreq*cd_cr
+print(wingdrag)
