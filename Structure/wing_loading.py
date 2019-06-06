410bccea18aef2b7b4af53e7416796b5e680e0aa
Omar <omar.mauri98@hotmail.com>
2019-06-06 11:13:22 +0200

    Added MOI estimation

--- Structure/wing_loading.py
+++ Structure/wing_loading.py
@@ -11,12 +11,12 @@ import power
 import matplotlib.pyplot as plt
 
 #DEFINE PARAMETERS
-def wing_load(static= False,show=False):
+def wing_load(static= False,show=True,grph=False):
     MTOW, Wing_w = mass_iteration(1630)
     cruiseT = power.ThrustCalculator(MTOW,aero_vals().vinfcr,1500 , 400000/aero_vals().vinfcr)
     cruiseL = Propellers(cruiseT.thrust, cruiseT.velocity,
                               cruiseT.rho, cruiseT.aero_vals.cl_cr, 0)
-    L =(MTOW * 9.81)/(wing_vals().b - 2.4) ####LINK TO VALUE WHEN INPUT
+    L =(MTOW * 9.81)/(wing_vals().b - 2.4)
     W_w = (Wing_w*9.81)/(wing_vals().b - 2.4)
     T_cp = cruiseL.thrustCP / cruiseL.numberCP
     T_hlp = cruiseL.thrustHLP / cruiseL.numberHLP
@@ -62,11 +62,11 @@ def wing_load(static= False,show=False):
             else:
                 mom = M_x -( W_hlp *max(0,ys[i]-y[0])) - W_hlp * max(0,ys[i]-y[1]) - R_z * ys[i] - W_hlp * max(0,ys[i] - y[2]) - W_hlp * max(0,ys[i]-y[3]) + L/2 *ys[i]**2 - W_w/2 * ys[i]**2
                 mom_x.append(mom)
-        plt.subplot(421)
-        plt.title('Moment Diagram (y-z plane)')
-        plt.xlabel('y [m]')
-        plt.ylabel('Internal Moment [Nm]')
-        plt.plot(ys,mom_x)
+        #plt.subplot(421)
+        #plt.title('Moment Diagram (y-z plane)')
+        #plt.xlabel('y [m]')
+        #plt.ylabel('Internal Moment [Nm]')
+        #plt.plot(ys,mom_x)
         #Z Moment Diagram
         mom_z = []
         for i in range(len(ys)):
@@ -76,19 +76,19 @@ def wing_load(static= False,show=False):
             else:
                 mom = M_z - T_hlp*max(0,ys[i]-y[0]) - T_hlp*max(0,ys[i]-y[1]) - T_hlp*max(0,ys[i]-y[2])-T_hlp*max(0,ys[i]-y[3]) - R_x * ys[i] + D/2 * ys[i]**2
                 mom_z.append(mom)
-        plt.subplot(422)
-        plt.title('Moment Diagram (x-y plane)')
-        plt.xlabel('y [m]')
-        plt.ylabel('Internal Moment [Nm]')
-        plt.plot(ys,mom_z)
+        #plt.subplot(422)
+        #plt.title('Moment Diagram (x-y plane)')
+        #plt.xlabel('y [m]')
+        #plt.ylabel('Internal Moment [Nm]')
+        #plt.plot(ys,mom_z)
         
         #Z Shear Diagram
         shr_z = []
         for i in range(len(ys)):
             shr = - W_hlp * y_ind[0][i] - W_hlp * y_ind[1][i] - W_hlp * y_ind[2][i] - R_z + L * ys[i] - W_w * ys[i] 
             shr_z.append(shr)
-        plt.subplot(423)
-        plt.plot(ys,shr_z)
+        #plt.subplot(423)
+        #plt.plot(ys,shr_z)
         #X Shear Diagram
         shr_x = []
         for i in range(len(ys)):
@@ -97,15 +97,35 @@ def wing_load(static= False,show=False):
         shradd = shr_x[0] - R_x
         for i in range(len(shr_x)):
             shr_x[i] = shr_x[i]-shradd 
-        plt.subplot(424)
-        plt.plot(ys,shr_x)
+        #plt.subplot(424)
+        #plt.plot(ys,shr_x)
         
         data =[ys,shr_z, shr_x , mom_x , mom_z]
-        
-        return data
+        if grph:
+             plt.subplot(421)
+             plt.title('Moment Diagram (y-z plane)')
+             plt.xlabel('y [m]')
+             plt.ylabel('Internal Moment [Nm]')
+             plt.plot(ys,mom_x)
+             plt.subplot(422)
+             plt.title('Moment Diagram (x-y plane)')
+             plt.xlabel('y [m]')
+             plt.ylabel('Internal Moment [Nm]')
+             plt.plot(ys,mom_z)
+             plt.subplot(423)
+             plt.title('Shear Diagram (y-z plane)')
+             plt.xlabel('y [m]')
+             plt.ylabel('Internal Shear [N]')
+             plt.plot(ys,shr_z)
+             plt.subplot(424)
+             plt.title('Shear Diagram (x-y plane)')
+             plt.xlabel('y [m]')
+             plt.ylabel('Internal Shear [N]')
+             plt.plot(ys,shr_x)
+        return data , grph
     
     
-def wing_deflec(data,E,I_xx,I_zz):
+def wing_deflec(data,grph,E,I_xx,I_zz):
     int_z_1=[]
     int_x_1=[]
     for i in range(len(data[3])-1):
@@ -121,10 +141,10 @@ def wing_deflec(data,E,I_xx,I_zz):
             int_x_1.append(sm_x)
     int_z_1.append(int_z_1[-1])
     int_x_1.append(int_x_1[-1])
-    plt.subplot(425)
-    plt.plot(data[0],int_z_1)
-    plt.subplot(426)
-    plt.plot(data[0],int_x_1)
+    #plt.subplot(425)
+    #plt.plot(data[0],int_z_1)
+    #plt.subplot(426)
+    #plt.plot(data[0],int_x_1)
     int_z_2 = []
     int_x_2 = []
     for i in range(len(data[3])-1):
@@ -144,11 +164,42 @@ def wing_deflec(data,E,I_xx,I_zz):
         int_x_2[i] = -int_x_2[i]/(E*I_xx)
     int_z_2.append(int_z_2[-1])
     int_x_2.append(int_x_2[-1])
-    
-    plt.subplot(427)
-    plt.plot(data[0],int_z_2)
-    plt.subplot(428)
-    plt.plot(data[0],int_x_2)
-a = wing_load(static=False,show=True)
+    #plt.subplot(427)
+    #plt.plot(data[0],int_z_2)
+    #plt.subplot(428)
+    #plt.plot(data[0],int_x_2)
+    if grph:
+        plt.subplot(425)
+        plt.plot(data[0],int_z_1)
+        plt.subplot(426)
+        plt.plot(data[0],int_x_1)
+        plt.subplot(427)
+        plt.plot(data[0],int_z_2)
+        plt.subplot(428)
+        plt.plot(data[0],int_x_2)
+    v_z = max(int_z_2)
+    v_x = max(int_x_2)
+    return v_z , v_x    
 
-b = wing_deflec(a,228e9,2.184e-4,6.739e-6)
+a = wing_load(grph=False)
+
+def inert_req(E,max_def,tol):
+    a = wing_load()
+    rnge = [0.0,1.0]
+    err=99999
+    while abs(err)>tol:
+        mid = (rnge[0] + rnge[1] )/2
+        defl = wing_deflec(a[0],a[1],E,6.5e-6,mid)
+        err=defl[0]-max_def
+        if err<0:
+            rnge[1] = mid
+        elif err>0:
+            rnge[0] = mid
+    return mid
+
+moi = inert_req(68.9e9,0.02,1e-9)
+        
+        
+#FINAL VALUE
+b = wing_deflec(a[0],a[1],228e9,2.184e-4,6.739e-6)
+
