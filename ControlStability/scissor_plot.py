30eb58ddabb3e705891902717d44afebf22c7a47
Tommy Tran <tommyvtran97@gmail.com>
2019-06-06 17:18:13 +0200

    Tail size shit v4

--- ControlStability/scissor_plot.py
+++ ControlStability/scissor_plot.py
@@ -42,6 +42,7 @@ def create_plot(x_cg):
     plt.plot([loading_diagram()[0], loading_diagram()[0]], [0, 1.0],'-yx')
     plt.plot([loading_diagram()[1], loading_diagram()[1]], [0, 1.0],'-yx')
     plt.axis([0, 0.4, 0, 1.0])
+    plt.grid()
     plt.show()
     pass
 
