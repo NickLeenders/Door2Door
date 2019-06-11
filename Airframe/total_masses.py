cc13e19a7566c91dab2d90395d63ada1b76d2018
nickleenders <nicklndrs@hotmail.com>
2019-06-11 17:30:50 +0200

    Fixed passenger mass

--- Airframe/total_masses.py
+++ Airframe/total_masses.py
@@ -1,6 +1,6 @@
 
 def MTOW_calculate(w_components):
-    MTOW = w_components.w_tail + w_components.w_structure + w_components.w_propeller + w_components.w_wing + w_components.w_drivetrain + w_components.w_fuel + w_components.w_cell + w_components.w_payload + 4.0*w_components.w_passenger[1] + w_components.w_battery + w_components.w_tank + w_components.w_cargo
+    MTOW = w_components.w_tail + w_components.w_structure + w_components.w_propeller + w_components.w_wing + w_components.w_drivetrain + w_components.w_fuel + w_components.w_cell + w_components.w_payload + 4.0*w_components.w_passenger + w_components.w_battery + w_components.w_tank + w_components.w_cargo
     return MTOW
 
 
